3a38cef04c1a58b8f2d851d2b626fff2c66a1783
ArthurSereda <neonbetcy@gmail.com>
2020-01-09 13:13:15 +0200

    Сделал таймер обратного отсчета

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
   "use sctrict";
+  //TABS
   let tab = document.querySelectorAll(".info-header-tab"),
     info = document.querySelector(".info-header"),
     tabContent = document.querySelectorAll(".info-tabcontent");
@@ -30,4 +31,49 @@ window.addEventListener("DOMContentLoaded", function() {
       }
     }
   });
+
+
+  //Timer
+  let deadLine = '2020-01-10';
+
+  function getRemainingTime(endTime) {
+    //получаем разницу в милисекундах между "сейчас" и "дедлайном"
+    let t = Date.parse(endTime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+        
+        return {
+          'total' : t,
+          'hours' : hours,
+          'minutes' : minutes,
+          'seconds' : seconds,
+        };
+  }
+
+  function setClock(id, endTime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+    //ведущий ноль в таймере
+    function leadZero(n) {
+      return  (n < 10 ? '0' : '') + n;
+    }
+
+    function updateClock() {
+      let t = getRemainingTime(endTime);
+      hours.textContent = leadZero(t.hours);
+      minutes.textContent = leadZero(t.minutes);
+      seconds.textContent = leadZero(t.seconds);
+      //Если тотал меньше 0 то таймер остановиться
+      if(t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('timer', deadLine);
+
 }); //window end
